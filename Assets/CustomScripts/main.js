e0e7135362c3021bf451955c9d24d3561745c017
Tinh Ngo <tinhngo@success-ss.com.vn>
2019-04-17 17:19:14 +0700

    implement title section

--- Assets/CustomScripts/main.js
+++ Assets/CustomScripts/main.js
@@ -1,6 +1,14 @@
 $(document).ready(function() {
   $("#slides").superslides({
-    animation: 'fade',
+    animation: "fade",
     play: 5000
+  });
+
+  var typed = new Typed(".typed", {
+    strings: ["Software Engineer.", "Web Developer.", "Freelancer."],
+    typeSpeed: 70,
+    loop: true,
+    startDelay: 1000,
+    showCursor: false
   });
 });
