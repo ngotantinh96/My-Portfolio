16e2a6579ffeca5eaf07753338df7b1fe63eda97
Tinh Ngo <tinhngo@success-ss.com.vn>
2019-04-18 11:55:02 +0700

    about section

--- app/Assets/CustomScripts/main.js
+++ app/Assets/CustomScripts/main.js
@@ -6,7 +6,12 @@ $(document).ready(function() {
   });
 
   var typed = new Typed(".typed", {
-    strings: ["Software Engineer.", "Web Developer.", "Freelancer."],
+    strings: [
+      "Software Engineer.",
+      "Web Developer.",
+      "Freelancer.",
+      "RPA Developer."
+    ],
     typeSpeed: 70,
     loop: true,
     startDelay: 1000,
