b64c3d90deb89727f8ee6e371d1fe7e44c35dd57
Tinh Ngo <tinhngo@success-ss.com.vn>
2019-04-19 10:15:02 +0700

    style for portfolio items and icons

--- app/Assets/CustomScripts/main.js
+++ app/Assets/CustomScripts/main.js
@@ -80,4 +80,13 @@ $(document).ready(function() {
   });
 
   $("[data-fancybox]").fancybox();
+
+  $(".items").isotope({
+    filter: "*",
+    animationOptions: {
+      duration: 1500,
+      easing: "linear",
+      queue: false
+    }
+  });
 });
